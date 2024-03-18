3f4ab46cba536dc45451ff2a57fc2d4bbc5bc4ac
PatykKon <115705372+PatykKon@users.noreply.github.com>
2024-03-18 13:28:08 +0100

    100commit | eighteenth commit - start adding security

---
+++ infrastructure/src/main/java/com/meeting/sport/app/token/TokenType.java
@@ -0,0 +1,5 @@
+package com.meeting.sport.app.token;
+
+public enum TokenType {
+    BEARER
+}
---
+++ infrastructure/src/main/java/com/meeting/sport/app/user/Token.java
@@ -0,0 +1,36 @@
+package com.meeting.sport.app.user;
+
+import com.meeting.sport.app.token.TokenType;
+import com.meeting.sport.app.user.UserEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table(name = "TOKEN")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Token {
+
+    @Id
+    @GeneratedValue
+    public Long id;
+
+    @Column(unique = true)
+    public String token;
+
+    @Enumerated(EnumType.STRING)
+    public TokenType tokenType = TokenType.BEARER;
+
+    public boolean revoked;
+
+    public boolean expired;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    public UserEntity user;
+}
