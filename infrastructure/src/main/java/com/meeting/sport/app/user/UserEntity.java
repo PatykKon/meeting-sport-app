87557a77b54ffc96d00844a97dfb686b76b7cd6f
PatykKon <115705372+PatykKon@users.noreply.github.com>
2024-03-19 12:00:01 +0100

    100commit | twentyth commit - start adding security

--- infrastructure/src/main/java/com/meeting/sport/app/user/UserEntity.java
+++ infrastructure/src/main/java/com/meeting/sport/app/user/UserEntity.java
@@ -3,8 +3,12 @@ package com.meeting.sport.app.user;
 import com.meeting.sport.app.sport_event.EventRoleEntity;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Table(name = "GAME_USER")
@@ -13,13 +17,57 @@ import java.util.List;
 @NoArgsConstructor
 @Builder
 @Getter
-public class UserEntity {
+public class UserEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
     private int age;
     @OneToMany(mappedBy = "userEntity")
     private List<EventRoleEntity> eventRoleEntities = new ArrayList<>();
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    @OneToMany(mappedBy = "user")
+    private List<Token> tokens;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true ;
+    }
 }
--- infrastructure/src/main/java/com/meeting/sport/app/user/UserRepositoryJPA.java
+++ infrastructure/src/main/java/com/meeting/sport/app/user/UserRepositoryJPA.java
@@ -1,6 +1,9 @@
 package com.meeting.sport.app.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
-interface UserRepositoryJPA extends JpaRepository<UserEntity, Long> {
+public interface UserRepositoryJPA extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByEmail(String email);
 }
