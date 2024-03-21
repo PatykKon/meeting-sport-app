3532b42a27d5ea1e94fc27bd84cbf749ce07d45a
PatykKon <115705372+PatykKon@users.noreply.github.com>
2024-03-21 18:59:15 +0100

    100commit | twenty-two commit - adding security

---
+++ configuration/src/main/java/com/meeting/sport/app/WebConfig.java
@@ -0,0 +1,18 @@
+package com.meeting.sport.app;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT")
+                .allowedHeaders("Content-Type", "Authorization")
+                .maxAge(3600);
+    }
+}
--- infrastructure/src/main/java/com/meeting/sport/app/token/TokenEntity.java
+++ infrastructure/src/main/java/com/meeting/sport/app/token/TokenEntity.java
@@ -19,17 +19,12 @@ public class TokenEntity {
     @Id
     @GeneratedValue
     public Long id;
-
     @Column(unique = true)
     public String token;
-
     @Enumerated(EnumType.STRING)
     public TokenType tokenType = TokenType.BEARER;
-
     public boolean revoked;
-
     public boolean expired;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     public UserEntity userEntity;
