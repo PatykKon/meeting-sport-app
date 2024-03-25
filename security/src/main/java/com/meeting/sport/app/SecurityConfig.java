4ce00ee188e305037b05ef70638249685abfbe64
PatykKon <115705372+PatykKon@users.noreply.github.com>
2024-03-25 18:09:49 +0100

    100commit | twenty-fifth commit - adding security

---
+++ security/src/main/java/com/meeting/sport/app/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.meeting.sport.app;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+
+import static com.meeting.sport.app.user.Role.ADMIN;
+import static com.meeting.sport.app.user.Role.OWNER;
+import static org.springframework.http.HttpMethod.*;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    private static final String[] WHITE_LIST_URL = {"/api/auth/**"};
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+    private final LogoutHandler logoutHandler;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req ->
+                        req.requestMatchers(WHITE_LIST_URL)
+                                .permitAll()
+                                .requestMatchers("/api/management/**").hasAnyRole(ADMIN.name(), OWNER.name())
+                                .requestMatchers(GET, "/api/management/**").hasAnyAuthority(ADMIN.name(), OWNER.name())
+                                .requestMatchers(POST, "/api/management/**").hasAnyAuthority(ADMIN.name(), OWNER.name())
+                                .requestMatchers(PUT, "/api/management/**").hasAnyAuthority(ADMIN.name(), OWNER.name())
+                                .requestMatchers(DELETE, "/api/management/**").hasAnyAuthority(ADMIN.name(), OWNER.name())
+                                .anyRequest()
+                                .authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .logout(logout ->
+                        logout.logoutUrl("/api/auth/logout")
+                                .addLogoutHandler(logoutHandler)
+                                .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext())
+                )
+        ;
+
+        return http.build();
+    }
+}
---
+++ security/src/main/java/com/meeting/sport/app/audtiting/ApplicationAuditAware.java
@@ -0,0 +1,30 @@
+package com.meeting.sport.app.audtiting;
+
+import com.meeting.sport.app.user.UserEntity;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class ApplicationAuditAware implements AuditorAware<Long> {
+    @Override
+    public Optional<Long> getCurrentAuditor() {
+        Authentication authentication =
+                SecurityContextHolder
+                        .getContext()
+                        .getAuthentication();
+        if (authentication == null ||
+                !authentication.isAuthenticated() ||
+                authentication instanceof AnonymousAuthenticationToken
+        ) {
+            return Optional.empty();
+        }
+
+        UserEntity userPrincipal = (UserEntity) authentication.getPrincipal();
+        return Optional.ofNullable(userPrincipal.getId());
+    }
+}
