ab110f16f2dc9826e27625d31f969a1fe70d6f87
PatykKon <115705372+PatykKon@users.noreply.github.com>
2024-04-09 16:42:23 +0200

    100commit | fourthy commit - add refaktor

--- domain/src/main/java/com/meeting/sport/app/sport_field/Address.java
+++ domain/src/main/java/com/meeting/sport/app/sport_field/Address.java
@@ -2,9 +2,9 @@ package com.meeting.sport.app.sport_field;
 
 
 public class Address {
-    String city;
-    String street;
-    String number;
+    private final String city;
+    private final String street;
+    private final String number;
 
     public Address(String city, String street, String number) {
         this.city = city;
