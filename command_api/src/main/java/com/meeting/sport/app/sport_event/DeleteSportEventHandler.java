d7f5c958bd8a6fff1a3d7e5ad1151e9bd1f56e32
PatykKon <115705372+PatykKon@users.noreply.github.com>
2024-05-08 12:40:42 +0200

    feature: delete-sport-event-handler -add miss handler

---
+++ command_api/src/main/java/com/meeting/sport/app/sport_event/DeleteSportEventHandler.java
@@ -0,0 +1,15 @@
+package com.meeting.sport.app.sport_event;
+git
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+class DeleteSportEventHandler  {
+    private final SportEventService sportEventService;
+
+    void handle(Long eventId,Long userId){
+
+        sportEventService.deleteSportEvent(eventId,userId);
+    }
+}
