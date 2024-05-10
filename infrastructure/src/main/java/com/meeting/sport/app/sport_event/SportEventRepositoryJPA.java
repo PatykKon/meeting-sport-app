941ec5e3d8e74965e0e33b82e456f2bed1b59095
PatykKon <115705372+PatykKon@users.noreply.github.com>
2024-05-10 13:50:19 +0200

    feature: status-creator-refactor: -refactor

--- infrastructure/src/main/java/com/meeting/sport/app/sport_event/SportEventRepositoryJPA.java
+++ infrastructure/src/main/java/com/meeting/sport/app/sport_event/SportEventRepositoryJPA.java
@@ -2,10 +2,13 @@ package com.meeting.sport.app.sport_event;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 interface SportEventRepositoryJPA extends JpaRepository<SportEventEntity, Long> {
 
     List<SportEventEntity> findAllBySportEventStatus(SportEventStatus sportEventStatus);
+
+    List<SportEventEntity> findAllByStartTimeAfter(LocalDateTime time);
 
 }
